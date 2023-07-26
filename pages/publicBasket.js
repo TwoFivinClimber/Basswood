852f294e71a9d23305e21d6c2a57fc6fe0542630
Brett Hughes <twofivinclimber@gmail.com>
2023-07-25 22:55:07 -0500

    goats page complete

--- pages/publicBasket.js
+++ pages/publicBasket.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-  Card, Container, Header, Image,
+  Button,
+  Card, Container, Divider, Header, Image,
 } from 'semantic-ui-react';
+import Link from 'next/link';
 import { getCurrentBasket } from '../utils/data/mergedData';
 import PublicVegCard from '../components/PublicVegCard';
 
@@ -12,23 +14,33 @@ function PublicBasket() {
     getCurrentBasket().then(setBasket);
   };
 
-  getTheBasket();
+  useEffect(() => {
+    getTheBasket();
+  }, []);
 
   return (
     <Container>
       <br />
-      <Header as="h1" style={{ fontSize: '3.5em' }} textAlign="center" content={basket.title} />
-      <Header as="h3" textAlign="center" content={`Week ${basket.week}`} />
-      <br />
+      <Header as="h1" style={{ fontSize: '3.5em' }} textAlign="center" content="This Weeks Basket" />
+      <Header as="h1" textAlign="center" content={`"${basket.title}"`} />
+      <Divider />
       <Image size="huge" centered src="/images/basket.jpg" />
       <br />
-      <Header as="h3">{basket.description}</Header>
+      <Header as="h2" content={`Week: ${basket.week}`} />
+      <p style={{ fontSize: '1.5em' }}>{basket.description}</p>
       <br />
       <Card.Group centered>
         {basket.veg?.map((veg) => (
           <PublicVegCard obj={veg} />
         ))}
       </Card.Group>
+      <Header as="h2" content="Not a Member ?" />
+      <p style={{ fontSize: '1.5em' }}>
+        Contact us to get on the list
+        <Link passHref href="/contact">
+          <Button color="orange" content="Here" size="large" style={{ marginLeft: '1em' }} />
+        </Link>
+      </p>
     </Container>
   );
 }
---
+++ utils/data/copy/goats.js
@@ -0,0 +1,17 @@
+/* eslint-disable max-len */
+const goatCopy = {
+  ourGoats: 'Our goats are great.  Except for when they get out and into the vegetables. Dairy goats are a valuable livestock breed cherished for their exceptional milk production. These goats are carefully bred and selected to yield rich and nutritious milk, making them indispensable for dairy farming. Their milk is not only high in protein and essential vitamins but is also more easily digestible compared to cow\'s milk, making it an excellent alternative for those with lactose intolerance. Dairy goats are known for their gentle temperament and adaptability, thriving in various climates and terrains. They play a vital role in providing an array of dairy products, such as cheese, yogurt, butter, and soap, contributing to the diversity and richness of the dairy industry.',
+  products: 'Check out the products we make with our dairy goat\'s milk!',
+};
+
+const goatFacts = ['Goats are versatile? Goats are incredibly versatile animals and can adapt to various farming systems. They can thrive in different climates and terrains, making them suitable for a wide range of agricultural settings.',
+  'Goats provide weed control? Goats are excellent foragers and can efficiently control weeds and brush on the farm. Their browsing behavior helps clear overgrown areas, reducing the need for manual weeding or herbicides.',
+  'Goats provide weed control? Goats are excellent foragers and can efficiently control weeds and brush on the farm. Their browsing behavior helps clear overgrown areas, reducing the need for manual weeding or herbicides.',
+  'Goats perform sustainable razing? Goats are known as "bio-mowers" as they graze on a wide variety of plants, including grasses, shrubs, and leaves. They can help maintain pastures, making grazing more sustainable and improving soil health.',
+  'Goats are great milk producers ? Dairy goat breeds are valuable for their milk production. Goat\'s milk is highly nutritious and is used to produce various dairy products like cheese, yogurt, and soap.',
+  'Goats produce fiber ? Some goat breeds, like Angora and Cashmere goats, produce luxurious fibers that are used to make mohair and cashmere wool. These fibers are highly prized in the textile industry.',
+  'Goats are low maintenance ? Goats are generally considered low-maintenance animals compared to other livestock. They are resourceful foragers and can be raised on a diverse diet, reducing the need for costly feed.',
+  'Goats utilize land ? Goats can utilize land that may not be suitable for other livestock or farming purposes. By grazing on less fertile or rough terrain, they can help make use of areas that might otherwise go unused.',
+  'Goats are natural fertalizers ?  Goat manure is a valuable natural fertilizer, rich in nutrients that can improve soil fertility and enhance crop growth.'];
+
+export { goatFacts, goatCopy };
